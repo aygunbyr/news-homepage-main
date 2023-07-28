04752c2a03b88fa5cb05971b3267bb0f838aa82d
Aygün Bayır <82970249+aygunbyr@users.noreply.github.com>
2023-07-28 21:48:55 +0300

    fix: better overlay

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const hamburgerMenuOpenButton = document.querySelector('.hamburger-menu-open');
 const hamburgerMenuCloseButton = document.querySelector('.close-icon');
+const overlay = document.querySelector('.overlay');
 
 hamburgerMenuOpenButton.addEventListener('click', () => {
   const mobileMenu = document.querySelector('.hamburger-menu-wrapper');
   mobileMenu.classList.add('open');
+  overlay.classList.add('open');
 });
 
 hamburgerMenuCloseButton.addEventListener('click', () => {
   const mobileMenu = document.querySelector('.hamburger-menu-wrapper');
   mobileMenu.classList.remove('open');
+  overlay.classList.remove('open');
 });
